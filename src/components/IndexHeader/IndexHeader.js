71c8a57d729ef77826e25c562f10b989318023c3
Adam Sanderson <Adam.Sanderson@theaccessgroup.com>
2019-02-10 10:55:09 +0000

    Add old data

--- src/components/IndexHeader/IndexHeader.js
+++ src/components/IndexHeader/IndexHeader.js
@@ -1,68 +1,45 @@
 import React from 'react'
 
 export const IndexHeader = () => {
-  return< header className="main-header {{#if @blog.cover}}" style={{backgroundImage: "url(img/xAnnie-Sanderson-Main-Blog-Picture-1.jpeg.pagespeed.ic.dYas82VPcR.webp)"}}>
-    {/* <nav className="main-nav overlay clearfix">
-        {{#if @blog.logo}}<a className="blog-logo" href="{{@blog.url}}"><img src="{{@blog.logo}}" alt="{{@blog.title}}" /></a>{{/if}}
-    </nav> */}
-    <div>
-        <div className="main-header-content inner">
-            <h1 className="page-title">Annie Sanderson</h1>
-            <h2 className="as-nav-links as-social">
-                <a href="/About/" className="fa-stack">
-                    <span className="as-social-bg fa fa-circle fa-stack-1x"></span>
-                    <span className="fa fa-circle-thin fa-stack-2x"></span>
-                    <span className="fa-stack-1x" style={{bottom:"4px"}}>me</span>
+  return <header class="main-header {{#if @blog.cover}}" style={{backgroundImage: "url(img/xAnnie-Sanderson-Main-Blog-Picture-1.jpeg.pagespeed.ic.dYas82VPcR.webp)"}}>
+  {/* <!-- <video class="main-header__video" src="https://s3-eu-west-1.amazonaws.com/anniesanderson-blog-uploads/Stone_720.webm" {{#if @blog.cover}} poster="url({{@blog.cover}})"{{/if}} autoplay="autoplay" loop="loop" muted="muted" preload="auto"></video>
+  <video class="main-header__video" autoplay="autoplay" id="video" loop="loop" muted="muted" preload="auto">
+    <source src="https://s3-eu-west-1.amazonaws.com/anniesanderson-blog-uploads/Stone_720.webm" type="video/webm">
+    <source src="https://s3-eu-west-1.amazonaws.com/anniesanderson-blog-uploads/Stone_480.webm" type="video/webm">
+    <source src="https://s3-eu-west-1.amazonaws.com/anniesanderson-blog-uploads/Stone_480.mp4" type="video/mp4">
+  </video> --> */}
+    <nav class="main-nav overlay clearfix">
+        {/* {{#if @blog.logo}}<a class="blog-logo" href="{{@blog.url}}"><img src="{{@blog.logo}}" alt="{{@blog.title}}" /></a>{{/if}} */}
+    </nav>
+    <div class="vertical">
+        <div class="main-header-content inner">
+            <h1 class="page-title">Annie Sanderson</h1>
+            <h2 class="as-nav-links as-social">
+                <a href="/About/" class="fa-stack">
+                    <span class="as-social-bg fa fa-circle fa-stack-1x"></span>
+                    <span class="fa fa-circle-thin fa-stack-2x"></span>
+                    <span class="fa-stack-1x" style={{bottom:"4px"}}>me</span>
                 </a>
-                <a href="mailto:annie_walton@hotmail.com" className="fa-stack">
-                    <span className="as-social-bg fa fa-circle fa-stack-1x"></span>
-                    <span className="fa fa-circle-thin fa-stack-2x"></span>
-                    <span className="fa fa-envelope-o fa-stack-1x"></span>
+                <a href="mailto:annie_walton@hotmail.com" class="fa-stack">
+                    <span class="as-social-bg fa fa-circle fa-stack-1x"></span>
+                    <span class="fa fa-circle-thin fa-stack-2x"></span>
+                    <span class="fa fa-envelope-o fa-stack-1x"></span>
                 </a>
-                <a href="http://twitter.com/annie_sanderson" className="fa-stack">
-                    <span className="as-social-bg fa fa-circle fa-stack-1x"></span>
-                    <span className="fa fa-circle-thin fa-stack-2x"></span>
-                    <span className="fa fa-twitter fa-stack-1x"></span>
+                <a href="http://twitter.com/annie_sanderson" class="fa-stack">
+                    <span class="as-social-bg fa fa-circle fa-stack-1x"></span>
+                    <span class="fa fa-circle-thin fa-stack-2x"></span>
+                    <span class="fa fa-twitter fa-stack-1x"></span>
                 </a>
-                <a href="https://instagram.com/anniesanderson80/" className="fa-stack">
-                    <span className="as-social-bg fa fa-circle fa-stack-1x"></span>
-                    <span className="fa fa-circle-thin fa-stack-2x"></span>
-                    <span className="fa fa-instagram fa-stack-1x"></span>
+                <a href="https://instagram.com/anniesanderson80/" class="fa-stack">
+                    <span class="as-social-bg fa fa-circle fa-stack-1x"></span>
+                    <span class="fa fa-circle-thin fa-stack-2x"></span>
+                    <span class="fa fa-instagram fa-stack-1x"></span>
                 </a>
             </h2>
-            {/* <h2 className="page-description">{{@blog.description}}</h2> */}
+            <h2 class="page-description"></h2>
         </div>
     </div>
-  </header>
-  {/*
+    <a class="scroll-down fa fa-chevron-down" href="#content" data-offset="-45"><span class="hidden">Scroll Down</span></a>
+</header>
 
-    <div className="vertical">
-        <div className="main-header-content inner">
-            <h1 className="page-title">{{@blog.title}}</h1>
-            <h2 className="as-nav-links as-social">
-                <a href="/About/" className="fa-stack">
-                    <span className="as-social-bg fa fa-circle fa-stack-1x"></span>
-                    <span className="fa fa-circle-thin fa-stack-2x"></span>
-                    <span className="fa-stack-1x" style="bottom:4px;">me</span>
-                </a>
-                <a href="mailto:annie_walton@hotmail.com" className="fa-stack">
-                    <span className="as-social-bg fa fa-circle fa-stack-1x"></span>
-                    <span className="fa fa-circle-thin fa-stack-2x"></span>
-                    <span className="fa fa-envelope-o fa-stack-1x"></span>
-                </a>
-                <a href="http://twitter.com/annie_sanderson" className="fa-stack">
-                    <span className="as-social-bg fa fa-circle fa-stack-1x"></span>
-                    <span className="fa fa-circle-thin fa-stack-2x"></span>
-                    <span className="fa fa-twitter fa-stack-1x"></span>
-                </a>
-                <a href="https://instagram.com/anniesanderson80/" className="fa-stack">
-                    <span className="as-social-bg fa fa-circle fa-stack-1x"></span>
-                    <span className="fa fa-circle-thin fa-stack-2x"></span>
-                    <span className="fa fa-instagram fa-stack-1x"></span>
-                </a>
-            </h2>
-            <h2 className="page-description">{{@blog.description}}</h2>
-        </div>
-    </div>
-    <a className="scroll-down fa fa-chevron-down" href="#content" data-offset="-45"><span className="hidden">Scroll Down</span></a> */}
 }
